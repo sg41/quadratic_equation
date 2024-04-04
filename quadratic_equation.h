02adfc4f15f652babf5225e53253ffe2794ada75
Joellebo <joellebo@student.21-school.ru>
2024-04-04 20:33:32 +0700

    docs

--- quadratic_equation.h
+++ quadratic_equation.h
@@ -7,5 +7,23 @@ typedef struct {
   double x2;
 } EquationResult;
 
+/**
+ * Solves a quadratic equation of the form ax^2 + bx + c = 0.
+ *
+ * @param a The coefficient of the x^2 term.
+ * @param b The coefficient of the x term.
+ * @param c The constant term.
+ *
+ * @return An EquationResult struct containing the number of roots and the
+ * values of the roots.
+ *
+ * The EquationResult struct has the following fields:
+ * - int number_of_roots: The number of real roots.
+ * - double x1: The value of the first root.
+ * - double x2: The value of the second root.
+ * If the number of roots is 0, the values of x1 and x2 are NAN.
+ * If the number of roots is 1, then x1 == x2, and contains the
+ * value of the root.
+ */
 EquationResult solve_equation(double a, double b, double c);
 #endif
